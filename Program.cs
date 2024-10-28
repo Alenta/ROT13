c60b5a2cecaa1693851b06748e51b663e85e3a06
Alenta <mef082024@gmail.com>
2024-10-28 10:04:12 +0100

    Init Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ROT13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SinglePermutations("Test");
+    }
+
+    public static List<string> SinglePermutations(string s)
+    {
+        return null;
+    }
+
+    public static string Rot13(string message)
+    {
+        // your code here
+        //message = message << 1;
+        return "";
+    }
+}
